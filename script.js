dda241681e7452426f863d5210688575b0989207
unknown <kskkorada76@gmail.com>
2023-10-29 21:31:02 +0530

    Improved JS code

--- script.js
+++ script.js
@@ -5,35 +5,34 @@ const body = document.querySelector("body");
 
 body.addEventListener("mouseleave", function () {
     cursor.classList.add("mouse-hidden");
-})
-body.addEventListener("mouseenter", function () {
+  });
+  
+  body.addEventListener("mouseenter", function () {
     cursor.classList.remove("mouse-hidden");
-})
-
-
-// cursor
-window.addEventListener("mousedown", () => {
+  });
+  
+  // cursor
+  window.addEventListener("mousedown", () => {
     cursor.classList.add("clicks");
-})
-
-window.addEventListener("mouseup", () => {
+  });
+  
+  window.addEventListener("mouseup", () => {
     cursor.classList.remove("clicks");
-})
-
-
-window.addEventListener("mousemove", (e) => {
-
+  });
+  
+  window.addEventListener("mousemove", (e) => {
     cursor.style.left = e.pageX + "px";
     cursor.style.top = e.pageY + "px";
-
-
-    links.forEach(link => {
-
-        link.addEventListener("mouseover", () => {
-            cursor.classList.add("hover");
-        })
-        link.addEventListener("mouseout", () => {
-            cursor.classList.remove("hover");
-        })
-    })
-})
+  });
+  
+  body.addEventListener("mouseover", (e) => {
+    if (e.target.tagName.toLowerCase() === "a") {
+      cursor.classList.add("hover");
+    }
+  });
+  
+  body.addEventListener("mouseout", (e) => {
+    if (e.target.tagName.toLowerCase() === "a") {
+      cursor.classList.remove("hover");
+    }
+  });
